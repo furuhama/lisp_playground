4a49faa301cdce4de8c4a27f567139a9dd5b5f11
furuhama <furuhama@users.noreply.github.com>
2018-09-23 15:09:26 +0900

    Update hello.scm

--- gauche/hello.scm
+++ gauche/hello.scm
@@ -1,28 +1,35 @@
 ;; String literal
+
 (print "hello, world") ; Hello, world
 
 ;; Starndard calculation
+
 (print (/ (* (+ 1 2)
              (- 10 8))
           2)) ; 3
 
 ;; Simple global argument definition (x <- 3)
+
 (define x 3)
 (print x) ; 3
 
 ;; List with single element
+
 (print 'x) ; x
 
 ;; Simple list definition literal
+
 (define lis1 '(1 2 3))
 (print lis1) ; (1 2 3)
 
 ;; Car & Cdr
+
 (print (car lis1)) ; 1
 (print (cdr lis1)) ; (2 3)
 (print (cdr (cdr lis1))) ; (3)
 
-;; List with dot annotation
+;; Pair with dot annotation
+
 (define lis2 '((1 . 2) . 3))
 (print lis2) ; ((1 . 2) . 3)
 
@@ -31,11 +38,21 @@
 (print (cdr (car lis2))) ; 2
 (print (cdr lis2)) ; 3
 
+;; Pair with `cons` function
+
+(define lis3 (cons 1 2))
+(print lis3) ; (1 . 2)
+
+(define lis4 (cons (cons 1 2) 3))
+(print lis4) ; ((1 . 2) . 3)
+
 ;; Global procedure definition
+
 (define (square x) (* x x))
 (print (square 19)) ; 361
 
 ;; Global prosedure definition with some other procedures
+
 (define (my-sqrt x)
   (define (average x y)
     (/ (+ x y) 2))
@@ -48,4 +65,5 @@
       guess
       (sqrt-iter (improve guess))))
   (sqrt-iter 1.0))
+
 (print (my-sqrt 5)) ; 2.2360688956433634
